57b77e64f26f0bde764f61e59d3e68519351a009
Abdelrahim Hentabli <abdelrahim.hentabli@gmail.com>
2020-04-21 00:53:20 -0700

    created a health bar controller

---
+++ Assets/Scenes/HUD.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HUD : MonoBehaviour
+{
+    public Slider Healthbar;
+    public int maxHealth;
+    public int currentHealth;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Healthbar.minValue = 0;
+        Healthbar.maxValue = maxHealth;
+        Healthbar.value = currentHealth;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Healthbar.minValue = 0;
+        Healthbar.maxValue = maxHealth;
+        Healthbar.value = currentHealth;
+    }
+}
