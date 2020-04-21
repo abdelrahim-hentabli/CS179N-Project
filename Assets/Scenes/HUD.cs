b0d6bbfadfc9f04abe66835c11e2df8be4e792b3
Abdelrahim Hentabli <abdelrahim.hentabli@gmail.com>
2020-04-21 11:22:31 -0700

    Added Health Potion Functionality

--- Assets/Scenes/HUD.cs
+++ Assets/Scenes/HUD.cs
@@ -3,14 +3,25 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+
+
 public class HUD : MonoBehaviour
 {
+    const int HEALTH_POTION_AMOUNT = 5;
     public Slider Healthbar;
+    public Text healthPotionAmount;
     public int maxHealth;
     public int currentHealth;
+    public int healthPotions;
+    public int potionStrength;
     // Start is called before the first frame update
     void Start()
     {
+        potionStrength = 35;
+        maxHealth = 100;
+        currentHealth = 100;
+        healthPotions = HEALTH_POTION_AMOUNT;
+        healthPotionAmount.text = healthPotions.ToString();
         Healthbar.minValue = 0;
         Healthbar.maxValue = maxHealth;
         Healthbar.value = currentHealth;
@@ -19,8 +30,41 @@ public class HUD : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Healthbar.minValue = 0;
-        Healthbar.maxValue = maxHealth;
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            onHealthPotion();
+        }
         Healthbar.value = currentHealth;
+    }
+
+    public void onHit(int damage)
+    {
+        currentHealth -= damage;
+        if(currentHealth <= 0)
+        {
+            currentHealth = 100;
+        }
+        Healthbar.value = currentHealth;
+    }
+
+    void  onHealthPotion()
+    {
+        if(healthPotions > 0)
+        {
+            healthPotions--;
+            currentHealth += potionStrength;
+            if(currentHealth > maxHealth)
+            {
+                currentHealth = maxHealth;
+            }
+            healthPotionAmount.text = healthPotions.ToString();
+            Healthbar.value = currentHealth;
+        }
+    }
+
+    public void replenishHealthPotions()
+    {
+        healthPotions = HEALTH_POTION_AMOUNT;
+        healthPotionAmount.text = healthPotions.ToString();
     }
 }
