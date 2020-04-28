eb1c08ebd52c4bc75504419ea271151eef86cd87
Abdelrahim Hentabli <abdelrahim.hentabli@gmail.com>
2020-04-27 19:43:06 -0700

    added bolt display and dynamic bomb counter

--- Assets/HUD/HUD.cs
+++ Assets/HUD/HUD.cs
@@ -7,21 +7,44 @@ using UnityEngine.UI;
 
 public class HUD : MonoBehaviour
 {
-    const int HEALTH_POTION_AMOUNT = 5;
+    const int HEALTH_POTION_STARTING_AMOUNT = 5;
+    const int BOMB_STARTING_AMOUNT = 3;
+    const int NUMBER_OF_QUICK_ITEMS = 1;
+    const int MAX_BOLTS = 6;
+
     public Slider Healthbar;
     public Text healthPotionAmount;
+    public Image[] bolts = new Image[6];
+
+    public Text bombAmount;
+
     public int maxHealth;
     public int currentHealth;
     public int healthPotions;
     public int potionStrength;
+
+    public int currentQuickItem;
+
+    public int bombs;
+
+    public int boltAmount;
+    
     // Start is called before the first frame update
     void Start()
     {
         potionStrength = 35;
         maxHealth = 100;
         currentHealth = 100;
-        healthPotions = HEALTH_POTION_AMOUNT;
+        healthPotions = HEALTH_POTION_STARTING_AMOUNT;
         healthPotionAmount.text = healthPotions.ToString();
+
+        currentQuickItem = 0;
+
+        bombs = BOMB_STARTING_AMOUNT;
+        bombAmount.text = bombs.ToString();
+
+        boltAmount = MAX_BOLTS;
+
         Healthbar.minValue = 0;
         Healthbar.maxValue = maxHealth;
         Healthbar.value = currentHealth;
@@ -34,6 +57,31 @@ public class HUD : MonoBehaviour
         {
             onHealthPotion();
         }
+
+        if (Input.GetKeyDown(KeyCode.K))
+        {
+            onCrossbow();
+        }
+
+
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            if(currentQuickItem == 0)
+            {
+                onBomb();
+            }
+        }
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            currentQuickItem++;
+            currentQuickItem %= NUMBER_OF_QUICK_ITEMS;
+            updateQuickItems();
+        }
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            onHit(22);
+        }
+
         Healthbar.value = currentHealth;
     }
 
@@ -47,7 +95,7 @@ public class HUD : MonoBehaviour
         Healthbar.value = currentHealth;
     }
 
-    void  onHealthPotion()
+    public void  onHealthPotion()
     {
         if(healthPotions > 0)
         {
@@ -62,9 +110,32 @@ public class HUD : MonoBehaviour
         }
     }
 
+    public void onBomb()
+    {
+        if(bombs > 0)
+        {
+            bombs--;
+            bombAmount.text = bombs.ToString();
+        }
+    }
+
     public void replenishHealthPotions()
     {
-        healthPotions = HEALTH_POTION_AMOUNT;
+        healthPotions = HEALTH_POTION_STARTING_AMOUNT;
         healthPotionAmount.text = healthPotions.ToString();
+    }
+
+    public void updateQuickItems()
+    {
+         
+    }
+
+    public void onCrossbow()
+    {
+        if(boltAmount > 0)
+        {
+            boltAmount--;
+            bolts[boltAmount].enabled = false;
+        }
     }
 }
