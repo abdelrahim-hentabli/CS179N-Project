96a69d9354f8a44194cdebf2a9af44d4828ff121
spure02 <43711455+spure02@users.noreply.github.com>
2020-05-18 15:09:56 -0700

    created animations for hit and death for fast melee

---
+++ Assets/Simi's Scripts/fastMelee_enemy.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class fastMelee_enemy : MonoBehaviour
+{
+    public Animator enemy;
+
+    public int maxHealth;
+    public static int currentHealth;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentHealth = maxHealth;
+    }
+
+    public void takeDamage(int damage)
+    {
+        currentHealth -= damage;
+
+        //Play hurt animation
+        enemy.SetTrigger("Hit");
+
+        if (currentHealth <= 0)
+        {
+            //Die
+            //Debug.Log("Enemy dead");
+
+            enemy.SetBool("Dead", true);
+            GetComponent<Collider2D>().enabled = false;
+            this.enabled = false;
+        }
+    }
