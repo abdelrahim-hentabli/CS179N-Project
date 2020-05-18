d83ac1e52e5b5454afe24ecab8ae4a03b3a4b9d0
spure02 <43711455+spure02@users.noreply.github.com>
2020-05-17 22:41:13 -0700

    finished seperate script for triggerAreaCheck, created empty script for hotzoneCheck

--- Assets/Simi's Scripts/fastMelee_triggerAreaCheck.cs
+++ Assets/Simi's Scripts/fastMelee_triggerAreaCheck.cs
@@ -2,7 +2,19 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class fastMelee_triggerAreaCheck : MonoBehaviour
-{
+public class fastMelee_triggerAreaCheck : MonoBehaviour {
     private fastMelee_behavior enemyParent;
+
+    private void Awake() {
+    	enemyParent = GetComponentInParent<fastMelee_behavior>();
+    }
+
+    private void OnTrigger2D(Collider2D collider) {
+    	if(collider.gameObject.CompareTag("Player")) {
+    		gameObject.SetActive(false);
+    		enemyParent.target = collider.transform;
+    		enemyParent.inRange = true;
+    		enemyParent.hotZone.SetActive(true);
+    	}
+    }
 }
