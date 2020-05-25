056e772c619076df46c84524a167d1e8a15b963e
deadmemeconnoisseur42 <aku006@ucr.edu>
2020-05-25 01:25:07 -0700

    Lava slows down player while they're in it

---
+++ Assets/Alex's Work/Alex's Scripts/Lava.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lava : MonoBehaviour
+{
+    GameObject thePlayer;
+    private PlayerController playerController;
+    public BoxCollider2D lavaTrigger;
+
+    private float damageTimer;
+    private bool inLava;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        thePlayer = GameObject.Find("Player");
+        playerController = thePlayer.GetComponent<PlayerController>();
+        inLava = false;
+        damageTimer = 0.0f;
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        if (inLava == true)
+        {
+            damageTimer += Time.deltaTime;
+            if (damageTimer >= 0.5f)
+            {
+                Debug.Log("Taking 5 damage!");
+                damageTimer = 0.0f;
+            }
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            Debug.Log("Stuck in lava!");
+            playerController.speed = 0.5f;
+            inLava = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            Debug.Log("Got out of lava!");
+            inLava = false;
+            damageTimer = 0.0f;
+            playerController.speed = 2.0f;
+        }
+    }
+}
