0cd327cd95374f8dcc24afb10bc1fbb36c762e23
deadmemeconnoisseur42 <aku006@ucr.edu>
2020-05-27 03:45:20 -0700

    Boulder follows a path, read description for more
    Boulder works by finding a destination and rolling towards it. As of now, this only works in one direction; the boulder cannot change directions yet.

--- Assets/Alex's Work/Alex's Scripts/Boulder.cs
+++ Assets/Alex's Work/Alex's Scripts/Boulder.cs
@@ -8,13 +8,57 @@ public class Boulder : MonoBehaviour
     public Animator anim;
     public CircleCollider2D trigger;
     public CircleCollider2D boulderCollider;
+    public Transform destination;
+    public float rollSpeed;
+    public bool grounded = false;
 
     // Start is called before the first frame update
     void Start()
     {
-
+        rollSpeed = 1.0f;
     }
 
+    void Update()
+    {
+        Roll();
+    }
+
+    //Determines whether boulder falls straight down or rolls towards the starting point
+    //Never rolls at a diagonal
+    void Roll()
+    {
+        if (grounded == true)
+        {
+            Debug.Log("Boulder rolling on ground!");
+            Vector2 destinationPosition = new Vector2(destination.position.x, transform.position.y);
+            transform.position = Vector2.MoveTowards(transform.position, destinationPosition, rollSpeed * Time.deltaTime);
+        }
+        else
+        {
+            Debug.Log("Boulder falling!");
+            Vector2 destinationPosition = new Vector2(0, -transform.position.y);
+            transform.position = Vector2.MoveTowards(transform.position, destinationPosition, rollSpeed * Time.deltaTime);
+        }
+    }
+
+    //Next two functions check if boulder is on ground or not
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.layer == 10) //10 is the index of ground; change this if ground layer's index is ever changed
+        {
+            grounded = true;
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.layer == 10)
+        {
+            grounded = false;
+        }
+    }
+
+    //When boulder runs into player, destroy its colliders and play break animation
     void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("Player"))
@@ -27,6 +71,7 @@ public class Boulder : MonoBehaviour
         }
     }
 
+    //Destroy the boulder object
     void DestroyBoulder()
     {
         Destroy(gameObject);
