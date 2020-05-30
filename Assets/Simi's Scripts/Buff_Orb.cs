3b7ca80fbe5c94ba8ee198a090bca53d1d7ae8cb
spure02 <43711455+spure02@users.noreply.github.com>
2020-05-30 06:37:50 -0700

    orb will dissapear once the player collides with it

--- Assets/Simi's Scripts/Buff_Orb.cs
+++ Assets/Simi's Scripts/Buff_Orb.cs
@@ -2,17 +2,21 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Buff_Orb : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+public class Buff_Orb : MonoBehaviour {
+	void OnTriggerEnter2D(Collider2D other) {
+		if(other.gameObject.CompareTag("Player")) {
+			Pickup();
+		}
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+	}
+
+	void Pickup() {
+		//generate effect
+
+		//apply effect to player
+
+		//remove orb
+		Destroy(gameObject);
+	}
+    
 }
