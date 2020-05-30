f0b55ffef2a5e4598077ae730f09dda212b2bcf6
spure02 <43711455+spure02@users.noreply.github.com>
2020-05-30 06:46:51 -0700

    player picking up orb will multiply attack damage by 2

--- Assets/Simi's Scripts/Buff_Orb.cs
+++ Assets/Simi's Scripts/Buff_Orb.cs
@@ -3,17 +3,21 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Buff_Orb : MonoBehaviour {
+	public int multiplier = 2;
+
 	void OnTriggerEnter2D(Collider2D other) {
 		if(other.gameObject.CompareTag("Player")) {
-			Pickup();
+			Pickup(other);
 		}
 
 	}
 
-	void Pickup() {
+	void Pickup(Collider2D player) {
 		//generate effect
 
 		//apply effect to player
+		Combat stats = player.GetComponent<Combat>();
+		stats.attackDamage *= multiplier;
 
 		//remove orb
 		Destroy(gameObject);
