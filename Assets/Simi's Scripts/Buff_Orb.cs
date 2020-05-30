ce37de75e67576cb890c4674765e3ee19e2c46f1
spure02 <43711455+spure02@users.noreply.github.com>
2020-05-30 07:10:47 -0700

    orb works as intended

--- Assets/Simi's Scripts/Buff_Orb.cs
+++ Assets/Simi's Scripts/Buff_Orb.cs
@@ -4,23 +4,32 @@ using UnityEngine;
 
 public class Buff_Orb : MonoBehaviour {
 	public int multiplier = 2;
+	//public float duration = 3.0f;
 
 	void OnTriggerEnter2D(Collider2D other) {
 		if(other.gameObject.CompareTag("Player")) {
-			Pickup(other);
+			StartCoroutine(Pickup(other));
 		}
 
 	}
 
-	void Pickup(Collider2D player) {
-		//generate effect
+	 IEnumerator Pickup(Collider2D player) {
+		//generate effect (not sure if this is needed yet)
 
-		//apply effect to player
+		//increase player damage by x2
 		Combat stats = player.GetComponent<Combat>();
-		stats.attackDamage *= multiplier;
+		stats.attackDamage = stats.attackDamage * multiplier;
 
-		//remove orb
+		//remove orb from scene
+		GetComponent<SpriteRenderer>().enabled = false;
+		GetComponent<Collider2D>().enabled = false;
+
+		//wait x amount of seconds, then reverse damage stats back to normal and destroy object
+		yield return new WaitForSeconds(10);
+		stats.attackDamage = stats.attackDamage / multiplier;
 		Destroy(gameObject);
+
+		
 	}
     
 }
