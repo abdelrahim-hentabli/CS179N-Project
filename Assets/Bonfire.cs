9fbdc1b5f7706d7fc57cba6ee5b029668d91a6b1
Abdelrahim Hentabli <abdelrahim.hentabli@gmail.com>
2020-05-31 18:59:17 -0700

    added saving and bonfire

---
+++ Assets/Bonfire.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bonfire : MonoBehaviour
+{
+    private float timer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        timer = 0.0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+        
+    }
+    void OnTriggerStay2D(Collider2D collision)
+    {
+        PlayerController playerController = collision.gameObject.GetComponent<PlayerController>();
+        if (playerController.isGrounded && playerController.isCrouched && timer >= 10f)
+        {
+            timer = 0.0f;
+            collision.gameObject.SendMessage("save");
+        }
+    }
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        PlayerController playerController = collision.gameObject.GetComponent<PlayerController>();
+        if (playerController.isGrounded && playerController.isCrouched && timer >= 10f)
+        {
+            timer = 0.0f;
+            collision.gameObject.SendMessage("save");
+        }
+    }
+}
--- Assets/HUD/HUD.cs
+++ Assets/HUD/HUD.cs
@@ -7,7 +7,7 @@ using UnityEngine.UI;
 
 public class HUD : MonoBehaviour
 {
-    const int HEALTH_POTION_STARTING_AMOUNT = 5;
+    public const int HEALTH_POTION_STARTING_AMOUNT = 5;
     const int BOMB_STARTING_AMOUNT = 3;
     const int SCROLL_STARTING_AMOUNT = 1;
     const int NUMBER_OF_QUICK_ITEMS = 2;
@@ -21,6 +21,7 @@ public class HUD : MonoBehaviour
     public Image[] bolts = new Image[6];
     public Image[] quickItems = new Image[2];
     public GameObject gameOverScreen;
+    public Text Saving;
 
     public GameObject[] hudElements = new GameObject[4];
 
@@ -41,9 +42,14 @@ public class HUD : MonoBehaviour
 
     public float currentLevelTime;
 
+    private bool saving;
+    private float savingTimer;
+
     // Start is called before the first frame update
     void Start()
     {
+        Saving.enabled = false;
+        saving = false;
         currentLevelTime = 0.0f;
         alive = true;
         gameOverScreen.SetActive(false);
@@ -75,6 +81,15 @@ public class HUD : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (saving)
+        {
+            savingTimer += Time.deltaTime;
+            if (savingTimer > 3.0f)
+            {
+                saving = false;
+                Saving.enabled = false;
+            }
+        }
         currentLevelTime += Time.deltaTime;
         int currentMinutes = (int)(currentLevelTime / 60);
         levelTimer.text = currentMinutes.ToString("00") +":" + (currentLevelTime - 60 * currentMinutes).ToString("00.00");
@@ -179,5 +194,26 @@ public class HUD : MonoBehaviour
         }
         gameOverScreen.SetActive(true);
         alive = false;
+    }
+
+    public void Refresh()
+    {
+        for (int i = 0; i < HUD_ELEMENTS; i++)
+        {
+            hudElements[i].SetActive(true);
+        }
+        gameOverScreen.SetActive(false);
+        alive = true;
+        quickItemAmount.text = itemAmount[currentQuickItem].ToString();
+        healthPotionAmount.text = healthPotions.ToString();
+        Healthbar.value = currentHealth;
+        int currentMinutes = (int)(currentLevelTime / 60);
+        levelTimer.text = currentMinutes.ToString("00") +":" + (currentLevelTime - 60 * currentMinutes).ToString("00.00");
+    }
+    public void save()
+    {
+        saving = true;
+        Saving.enabled = true;
+        savingTimer = 0.0f;
     }
 }
