755621af5dc46db3e4d2aef2ddcd35e1bccb131e
alira008 <alira008@ucr.edu>
2020-06-01 21:18:33 -0700

    Added a projectile for the Flying Skull enemy with animations

---
+++ Assets/Enemies/Flying_Skull_Enemy/Scripts/Flame_Projectile.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flame_Projectile : MonoBehaviour
+{
+	public float speed;
+	public Rigidbody2D rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb.velocity = -transform.right * speed;
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {	
+    	if (collision.gameObject.tag == "PlayerHitbox" || collision.gameObject.layer == 13 || collision.gameObject.tag == "Wall")	
+    	{
+    		Destroy(gameObject);
+    	}
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+    	// Layer 13 is Wall layer
+    	if(collision.gameObject.layer == 13)
+    	{
+    		Destroy(gameObject);
+    	}
+    }
+
+}
