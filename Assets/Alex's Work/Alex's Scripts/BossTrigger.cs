cce26f248ab8a796617d46da88dd1fd89e0926f7
Abdelrahim Hentabli <abdelrahim.hentabli@gmail.com>
2020-06-09 15:02:46 -0700

    github for unity made me commit them again

---
+++ Assets/Alex's Work/Alex's Scripts/BossTrigger.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossTrigger : MonoBehaviour
+{
+    private BossBehavior bossBehavior;
+
+    private void Awake()
+    {
+        bossBehavior = GetComponentInParent<BossBehavior>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            gameObject.SetActive(false);
+            bossBehavior.target = collision.transform;
+            bossBehavior.inRange = true;
+            bossBehavior.hotzone.SetActive(true);
+        }
+    }
+}
