9202f73db65608f4bd4fd0e1926855c58fe117fa
alira008 <alira008@ucr.edu>
2020-06-10 10:43:51 -0700

    Bug fixes
    - fixed not being able to enter boss level

--- Assets/Simi's Scripts/enterBossLevel.cs
+++ Assets/Simi's Scripts/enterBossLevel.cs
@@ -5,14 +5,14 @@ using UnityEngine.SceneManagement;
 
 public class enterBossLevel : MonoBehaviour {
 	void OnTriggerStay2D(Collider2D player) {
-        if(player.gameObject.CompareTag("Player") && Input.GetKeyDown(KeyCode.E)) {
+        if(player.gameObject.tag == "Player" && Input.GetKeyDown(KeyCode.E)) {
     		SceneManager.LoadScene("BossScene");
     		Debug.Log("Loading...");
     	}
     }
 
 	void OnTriggerEnter2D(Collider2D player) {
-    	if(player.gameObject.CompareTag("Player") && Input.GetKeyDown(KeyCode.E)) {
+    	if(player.gameObject.tag == "Player" && Input.GetKeyDown(KeyCode.E)) {
     		SceneManager.LoadScene("BossScene");
     		Debug.Log("Loading...");
     	}
