4bf6b57193d8b6eca0230c1f4c77662b47ec01e6
spure02 <43711455+spure02@users.noreply.github.com>
2020-06-10 07:11:44 -0700

    added script to enter boss level

---
+++ Assets/Simi's Scripts/enterBossLevel.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enterBossLevel : MonoBehaviour {
+	void OnTriggerEnter2D(Collider2D player) {
+    	if(player.gameObject.CompareTag("Player") && Input.GetKeyDown(KeyCode.E)) {
+    		SceneManager.LoadScene("BossScene");
+    		Debug.Log("Loading...");
+    	}
+    }
+    
+}
