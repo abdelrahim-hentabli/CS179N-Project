dc2139ce43c466c9d03526f50abb6b25a629c1c6
spure02 <43711455+spure02@users.noreply.github.com>
2020-06-11 15:45:52 -0700

    scroll item can now be picked up, need to integrate into quick items now

---
+++ Assets/Simi's Scripts/scroll_item.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scroll_item : MonoBehaviour {
+    void OnTriggerEnter2D(Collider2D other) {
+		if(other.gameObject.CompareTag("Player")) {
+            //other.SendMessage("giveBuffItem");
+            Pickup();
+		}
+
+	}
+
+	void Pickup() {
+		GetComponent<SpriteRenderer>().enabled = false;
+		GetComponent<Collider2D>().enabled = false;
+    	Destroy(gameObject);
+
+	}
+}
